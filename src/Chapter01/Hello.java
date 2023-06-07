39a3a72fa9326f0d42ef2b151f9c512e72d9a3ac
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-07 14:59:49 +0900

    Chapter 02 変数 一日目

---
+++ src/Chapter01/Hello.java
@@ -0,0 +1,7 @@
+package Chapter01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // 画面に文字を出力する。
+    }
+}
---
+++ src/Chapter02/VarEx1.java
@@ -0,0 +1,14 @@
+package Chapter02;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000;  // 変数 ageの値に2000を加えて変数yearに保存
+        age = age + 1;      // 変数 ageに保存された値を1増加
+    }
+}
